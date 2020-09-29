caa4a34c1b1028d443a51f0d55d97d65af0b4712
theodor1289 <amariucaitheodor@gmail.com>
2020-09-29 12:48:08 +0100

    lambda function now working

---
+++ lambda-benchmarking/src/client/client.go
@@ -0,0 +1,7 @@
+package main
+
+// /benchmarking?ExecMilliseconds=50
+// Endpoint request URI: https://lambda.eu-west-2.amazonaws.com/2015-03-31/functions/arn:aws:lambda:eu-west-2:335329526041:function:benchmarking/invocations
+func main() {
+	
+}
