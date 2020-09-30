b19cbdbb26bdb4da92048e2b4294aa6bedca0df3
theodor1289 <amariucaitheodor@gmail.com>
2020-09-30 14:02:52 +0100

    added burst functionality and improved goroutines synchronization

--- lambda-benchmarking/client/src/benchmarking/writer.go
+++ lambda-benchmarking/client/src/benchmarking/writer.go
@@ -14,21 +14,27 @@ type SafeWriter struct {
 	mux    sync.Mutex
 }
 
-func (writer *SafeWriter) GenerateLatencyRecord(reqChannel chan<- int, id int, execMilliseconds int, payloadLength int) {
+func (writer *SafeWriter) GenerateLatencyRecord(requestsWaitGroup *sync.WaitGroup, execMilliseconds int, payloadLength int, burstId int) {
+	defer requestsWaitGroup.Done()
 	start := time.Now()
 
-	log.Printf("Making request with id %d to API Gateway", id)
 	writer.WriteRowToFile(
 		networking.CallAPIGateway(execMilliseconds, payloadLength),
+		start.Format(time.StampNano),
+		time.Now().Format(time.StampNano),
 		strconv.FormatInt(time.Since(start).Milliseconds(), 10),
+		strconv.Itoa(burstId),
 	)
-
-	reqChannel <- id
 }
 
-func (writer *SafeWriter) WriteRowToFile(column1 string, column2 string) {
+func (writer *SafeWriter) WriteRowToFile(
+	AwsRequestID string,
+	SentAt string,
+	ReceivedAt string,
+	ClientLatencyMs string,
+	BurstID string) {
 	writer.mux.Lock()
-	if err := writer.Writer.Write([]string{column1, column2}); err != nil {
+	if err := writer.Writer.Write([]string{AwsRequestID, SentAt, ReceivedAt, ClientLatencyMs, BurstID}); err != nil {
 		log.Fatal(err)
 	}
 	writer.mux.Unlock()
