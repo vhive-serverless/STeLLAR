cd0065af6942fd20d90cae040e497654841d3a5a
theodor1289 <amariucaitheodor@gmail.com>
2020-10-05 21:23:03 +0100

    added empirical CDFs to client functionality

--- lambda-benchmarking/client/src/benchmarking/deltas.go
+++ lambda-benchmarking/client/src/benchmarking/deltas.go
@@ -1,6 +1,8 @@
 package benchmarking
 
 import (
+	"math"
+	"math/rand"
 	"time"
 )
 
@@ -13,19 +15,28 @@ func MakeBurstDeltasRelative(burstDeltas []time.Duration) []time.Duration {
 	return burstRelativeDeltas
 }
 
-func CreateBurstDeltas(frequencySeconds int, burstsNumber int) []time.Duration {
+func CreateBurstDeltas(frequencySeconds int, burstsNumber int, randomization bool) []time.Duration {
 	var burstDeltas []time.Duration
 	if frequencySeconds != -1 {
 		// latency profiler run, delta is constant
 		burstDeltas = make([]time.Duration, burstsNumber)
 		for i := range burstDeltas {
-			burstDeltas[i] = time.Duration(frequencySeconds) * time.Second
+			if randomization {
+				// scale and shift the standard normal distribution by frequencySeconds
+				// make sure result is positive
+				sampleBurst := math.Max(rand.NormFloat64()*float64(frequencySeconds)+float64(frequencySeconds), 0)
+				burstDeltas[i] = time.Duration(sampleBurst * 1000) * time.Millisecond
+			} else {
+				burstDeltas[i] = time.Duration(frequencySeconds) * time.Second
+			}
 		}
 	} else {
 		// cold start delta identifier run, delta varies so that the exact timeout can be identified
 		burstDeltas = []time.Duration{
-			time.Duration(0),
+			time.Duration(0), // COLD START
 			500 * time.Millisecond,
+			time.Second, // WAIT A BIT MORE FOR INITIALIZATION
+			time.Second,
 			time.Second,
 			5 * time.Second,
 			15 * time.Second,
--- lambda-benchmarking/client/src/networking/client.go
+++ lambda-benchmarking/client/src/networking/client.go
@@ -15,10 +15,10 @@ const (
 	timeout = 8 * time.Second
 )
 
-func CallAPIGateway(lambdaIncrementLimit int, payloadLengthBytes int) string {
+func CallAPIGateway(gatewayEndpoint string, lambdaIncrementLimit int, payloadLengthBytes int) string {
 	req, err := http.NewRequest("GET",
 		fmt.Sprintf("%s/benchmarking?LambdaIncrementLimit=%d&PayloadLengthBytes=%d",
-			CheckAndReturnEnvVar("AWS_API_GATEWAY_ENDPOINT"),
+			gatewayEndpoint,
 			lambdaIncrementLimit,
 			payloadLengthBytes,
 		),
