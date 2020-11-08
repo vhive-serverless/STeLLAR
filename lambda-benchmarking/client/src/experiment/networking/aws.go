fc2027300ac032c381d6265ef1b742283840fac7
theodor1289 <amariucaitheodor@gmail.com>
2020-11-08 17:32:39 +0000

    fixed default config bug, removed redundant infra code

--- lambda-benchmarking/client/src/experiment/networking/aws.go
+++ lambda-benchmarking/client/src/experiment/networking/aws.go
@@ -3,11 +3,9 @@ package networking
 import (
 	"encoding/json"
 	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/aws/credentials"
 	"github.com/aws/aws-sdk-go/aws/session"
 	v4 "github.com/aws/aws-sdk-go/aws/signer/v4"
 	log "github.com/sirupsen/logrus"
-	"os"
 )
 
 const awsRegion = "us-west-1"
@@ -20,22 +18,10 @@ func getAWSSignerSingleton() *v4.Signer {
 	}
 
 	sessionInstance := session.Must(session.NewSession(&aws.Config{
-		Credentials: credentials.NewStaticCredentials(
-			checkAndReturnEnvVar("AWS_ACCESS_KEY_ID"),
-			checkAndReturnEnvVar("AWS_SECRET_ACCESS_KEY"),
-			""),
 		Region: aws.String(awsRegion),
 	}))
 	signerSingleton = v4.NewSigner(sessionInstance.Config.Credentials)
 	return signerSingleton
-}
-
-func checkAndReturnEnvVar(key string) string {
-	envVar, isSet := os.LookupEnv(key)
-	if !isSet {
-		log.Warnf("Environment variable %s is not set.", key)
-	}
-	return envVar
 }
 
 type lambdaFunctionResponse struct {
