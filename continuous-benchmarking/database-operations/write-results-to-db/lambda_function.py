307ae3342da2a43b0f6266f6ee0c18973908252e
dilinade <dilinade@users.noreply.github.com>
2022-09-22 12:46:45 +0100

    Added copyrights and minor refactoring

--- continuous-benchmarking/database-operations/write-results-to-db/lambda_function.py
+++ continuous-benchmarking/database-operations/write-results-to-db/lambda_function.py
@@ -1,3 +1,25 @@
+# MIT License
+#
+# Copyright (c) 2022 Dilina Dehigama and EASE Lab
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
 import json
 import logging
 import os
@@ -29,42 +51,26 @@ def lambda_handler(event, context):
         
     timestamp = str(time.time())
 
-    table = dynamodb.Table('continous_results')
-    
-    # Extract params from body
-    subtype = data.get('subtype')
-    minimum = data.get('min')
-    maximum = data.get('max')
-    median = data.get('median')
-    tail_latency = data.get('tail_latency')
-    first_quartile = data.get('first_quartile')
-    third_quartile = data.get('third_quartile')
-    standard_deviation = data.get('standard_deviation')
-    payload_size = data.get('payload_size')
-    burst_size = data.get('burst_size')
-    IAT_type = data.get('IAT_type')
-    count = data.get('count')
-    date = data.get('date')
-    provider = data.get('provider')
+    table = dynamodb.Table('continuous_results')
 
     # Create a table entry
     entry = {
         'id': str(uuid.uuid1()),
         'experiment_type': experiment_type,
-        'subtype': subtype,
-        'min': minimum,
-        'max': maximum,
-        'median': median,
-        'tail_latency': tail_latency,
-        'first_quartile': first_quartile,
-        'third_quartile': third_quartile,
-        'standard_deviation': standard_deviation,
-        'payload_size': payload_size,
-        'burst_size': burst_size,
-        'IAT_type': IAT_type,
-        'count': count,
+        'subtype': data.get('subtype'),
+        'min': data.get('min'),
+        'max': data.get('max'),
+        'median': data.get('median'),
+        'tail_latency': data.get('tail_latency'),
+        'first_quartile': data.get('first_quartile'),
+        'third_quartile': data.get('third_quartile'),
+        'standard_deviation': data.get('standard_deviation'),
+        'payload_size': data.get('payload_size'),
+        'burst_size': data.get('burst_size'),
+        'IAT_type': data.get('IAT_type'),
+        'count': data.get('count'),
         'date': date,
-        'provider': provider,
+        'provider': data.get('provider'),
         'createdAt': timestamp
     }
 
