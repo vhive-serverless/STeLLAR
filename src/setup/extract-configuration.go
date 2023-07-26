4d584f0b8370acf400fab176c434e6049511d1ec
MINK <76023265+kk-min@users.noreply.github.com>
2023-07-26 18:55:48 +0800

    Add new fields to SubExperiment struct

--- src/setup/extract-configuration.go
+++ src/setup/extract-configuration.go
@@ -29,20 +29,20 @@ import (
 	"stellar/util"
 )
 
-//Configuration is the schema for all experiment configurations.
+// Configuration is the schema for all experiment configurations.
 type Configuration struct {
 	Sequential     bool            `json:"Sequential"`
 	Provider       string          `json:"Provider"`
 	SubExperiments []SubExperiment `json:"SubExperiments"`
 }
 
-//EndpointInfo contains an ID identifying the function together with the IDs of other functions further in the data transfer chain
+// EndpointInfo contains an ID identifying the function together with the IDs of other functions further in the data transfer chain
 type EndpointInfo struct {
 	ID                   string
 	DataTransferChainIDs []string
 }
 
-//SubExperiment contains all the information needed for a sub-experiment to run.
+// SubExperiment contains all the information needed for a sub-experiment to run.
 type SubExperiment struct {
 	ID                      int
 	Title                   string   `json:"Title"`
@@ -60,6 +60,8 @@ type SubExperiment struct {
 	FunctionImageSizeMB     float64  `json:"FunctionImageSizeMB"`
 	DataTransferChainLength int      `json:"DataTransferChainLength"`
 	StorageTransfer         bool     `json:"StorageTransfer"`
+	Handler                 string   `json:"Handler"`
+	PackagePattern          string   `json:"PackagePattern"`
 	// All of the below are computed after reading the configuration
 	BusySpinIncrements []int64 `json:"BusySpinIncrements"`
 	Endpoints          []EndpointInfo
@@ -76,12 +78,13 @@ const (
 	defaultProvider                = "aws"
 	defaultFunction                = "producer-consumer"
 	defaultPackageType             = "Zip"
+	defaultPackagePattern          = "**"
 	defaultParallelism             = 1
 	defaultDataTransferChainLength = 1
 	defaultFunctionMemoryMB        = 128
 )
 
-//ExtractConfiguration will read and parse the JSON configuration file, assign any default values and return the config object
+// ExtractConfiguration will read and parse the JSON configuration file, assign any default values and return the config object
 func ExtractConfiguration(configFilePath string) Configuration {
 	configFile := util.ReadFile(configFilePath)
 	configByteValue, _ := io.ReadAll(configFile)
