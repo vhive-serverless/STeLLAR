6e1e62b9d69198aa1df1247dcf52f21beb4385b2
Ria <maja.durackova@gmail.com>
2023-08-01 13:41:30 +0100

    add boiler plate

--- src/setup/run.go
+++ src/setup/run.go
@@ -27,6 +27,7 @@ package setup
 import (
 	log "github.com/sirupsen/logrus"
 	"os"
+	"stellar/setup/deployment/building"
 	"stellar/setup/building"
 	code_generation "stellar/setup/code-generation"
 	"stellar/setup/deployment/connection"
