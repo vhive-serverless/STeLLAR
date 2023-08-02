2c37f8fb496907094691f51c46b535572b8bb18c
Ria <maja.durackova@gmail.com>
2023-08-02 16:08:10 +0100

    reformat lambda_function.py format and fix typo in text file name

--- src/setup/deployment/raw-code/serverless/aws/hellopy/lambda_function.py
+++ src/setup/deployment/raw-code/serverless/aws/hellopy/lambda_function.py
@@ -7,34 +7,34 @@ import time
 
 def lambda_handler(request, context):
 
-incr_limit = 0
+    incr_limit = 0
 
-if 'queryStringParameters' in request and 'IncrementLimit' in request['queryStringParameters']:
-incr_limit = int(request['queryStringParameters'].get('IncrementLimit', 0))
-elif 'body' in request and json.loads(request['body'])['IncrementLimit']:
-incr_limit = int(json.loads(request['body'])['IncrementLimit'])
+    if(request['queryStringParameters'] and 'IncrementLimit' in request['queryStringParameters']):
+        incr_limit = int(request['queryStringParameters'].get('IncrementLimit', 0))
+    elif request['body'] and json.loads(request['body'])['IncrementLimit']:
+        incr_limit = int(json.loads(request['body'])['IncrementLimit'])
 
-simulate_work(incr_limit)
+    simulate_work(incr_limit)
 
-json_region = os.environ.get('AWS_REGION','Unknown')
+    json_region = os.environ.get('AWS_REGION','Unknown')
 
-response = {
-"statusCode": 200,
-"headers": {
-"Content-Type": "application/json"
-},
-"body": json.dumps({
-"Region ": json_region,
-"RequestID": context.aws_request_id,
-"TimestampChain": [str(time.time_ns())]
-},indent=4)
-}
+    response = {
+        "statusCode": 200,
+        "headers": {
+            "Content-Type": "application/json"
+        },
+        "body": json.dumps({
+            "Region ": json_region,
+            "RequestID": context.aws_request_id,
+            "TimestampChain": [str(time.time_ns())]
+        },indent=4)
+    }
 
-return response
+    return response
 
 
 def simulate_work(increment):
-# MAXNUM = 6103705
-num = 0
-while num < increment:
-num += 1
+    # MAXNUM = 6103705
+    num = 0
+    while num < increment:
+        num += 1
