a2fe4fab86d2437b3186490298c5cb10889ecc7e
Ria <maja.durackova@gmail.com>
2023-08-03 15:18:35 +0100

    add code-generator.go and update README (also other minor changes)

--- src/setup/run.go
+++ src/setup/run.go
@@ -28,6 +28,7 @@ import (
 	log "github.com/sirupsen/logrus"
 	"os"
 	"stellar/setup/building"
+	code_generation "stellar/setup/code-generation"
 	"stellar/setup/deployment/connection"
 	"stellar/setup/deployment/connection/amazon"
 	"time"
@@ -85,50 +86,28 @@ func ProvisionFunctions(config Configuration) {
 
 //ProvisionFunctions will deploy, reconfigure, etc. functions to get ready for the sub-experiments.
 func ProvisionFunctionsServerless(config Configuration) {
-	const (
-		nicContentionWarnThreshold = 800 // Experimentally found
-		storageSpaceWarnThreshold  = 500 // 500 * ~18KiB = 10MB just for 1 sub-experiment
-	)
 
 	slsConfig := &Serverless{}
 	builder := &building.Builder{}
 
 	for index, subExperiment := range config.SubExperiments {
-		config.SubExperiments[index].ID = index
 
-		for _, burstSize := range subExperiment.BurstSizes {
-			if burstSize > nicContentionWarnThreshold {
-				log.Warnf("Experiment %d has a burst of size %d, NIC (Network Interface Controller) contention may occur.",
-					index, burstSize)
-				if !promptForBool("Do you wish to continue?") {
-					os.Exit(0)
-				}
-			}
-		}
+		slsConfig.AddFunctionConfig(&subExperiment, index)
 
-		if subExperiment.Bursts >= storageSpaceWarnThreshold &&
-			(subExperiment.Visualization == "all" || subExperiment.Visualization == "histogram") {
-			log.Warnf("SubExperiment %d is generating histograms for each burst, this will create a large number (%d) of new files (>10MB).",
-				index, subExperiment.Bursts)
-			if !promptForBool("Do you wish to continue?") {
-				os.Exit(0)
-			}
-		}
-
-		slsConfig.AddFunctionConfig()
+		//TODO: generate the code
+		code_generation.GenerateCode(subExperiment.Function, config.Provider)
 
 		// TODO: build the functions (Java and Golang)
 		builder.BuildFunction("", "")
 
-		// TODO: Create filler files here and do the zipping if necessary. Define
+		// TODO: Create filler files here and do the zipping if necessary.
 		// Use deployment.generateFillerFile() function
 		// Use the packaging.GenerateZIP() function
 	}
 
 	slsDeployMessage := DeployService()
-	log.Info(slsDeployMessage)
+	log.Warn(slsDeployMessage)
 
 	// TODO: assign endpoints to subexperiments
 	// Get the endpoints by scraping the serverless deploy message.
-
 }
