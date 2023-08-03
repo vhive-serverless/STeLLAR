645076cdd85d9914742246a1d1a92bcca29c806d
Ria <maja.durackova@gmail.com>
2023-08-03 19:17:41 +0100

    add CI pipeline and add code generator test which I forgot to add last time

--- src/main.go
+++ src/main.go
@@ -42,7 +42,7 @@ var configPathFlag = flag.String("c", "experiments/tests/aws/data-transfer.json"
 var endpointsDirectoryPathFlag = flag.String("g", "endpoints", "Directory containing provider endpoints to be used.")
 var specificExperimentFlag = flag.Int("r", -1, "Only run this particular experiment.")
 var logLevelFlag = flag.String("l", "info", "Select logging level.")
-var serverlessDeployment = flag.Bool("s", false, "Use serverless.com framework for deployment. ")
+var serverlessDeployment = flag.Bool("s", true, "Use serverless.com framework for deployment. ")
 
 func main() {
 	startTime := time.Now()
@@ -79,9 +79,8 @@ func main() {
 		setup.ProvisionFunctionsServerless(config)
 	} else {
 		setup.ProvisionFunctions(config)
+		benchmarking.TriggerSubExperiments(config, outputDirectoryPath, *specificExperimentFlag)
 	}
-
-	benchmarking.TriggerSubExperiments(config, outputDirectoryPath, *specificExperimentFlag)
 
 	// Remove the functions used for experiments, since they are no longer needed
 	if *serverlessDeployment {
