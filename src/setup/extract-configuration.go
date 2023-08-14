f9b4db32483d3c5bc7831135917583e315388f7b
kk-min <76023265+kk-min@users.noreply.github.com>
2023-08-14 17:17:28 +0800

    Add default runtime for top level configuration

--- src/setup/extract-configuration.go
+++ src/setup/extract-configuration.go
@@ -99,6 +99,9 @@ func ExtractConfiguration(configFilePath string) Configuration {
 	if parsedConfig.Provider == "" {
 		parsedConfig.Provider = defaultProvider
 	}
+	if parsedConfig.Runtime == "" {
+		parsedConfig.Runtime = defaultRuntime
+	}
 
 	for index := range parsedConfig.SubExperiments {
 		if parsedConfig.SubExperiments[index].Function == "" {
