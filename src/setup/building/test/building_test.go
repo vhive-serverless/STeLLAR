370597437915f28383f10bae458503e892bcdb72
Yi Pun Wong <wongyipun99@gmail.com>
2023-08-16 15:11:23 +0800

    update unit tests

--- src/setup/building/test/building_test.go
+++ src/setup/building/test/building_test.go
@@ -1,7 +1,6 @@
 package building
 
 import (
-	"bytes"
 	"github.com/stretchr/testify/assert"
 	"os"
 	"stellar/setup/building"
@@ -16,14 +15,8 @@ func TestBuildFunctionJava(t *testing.T) {
 func TestBuildFunctionGolang(t *testing.T) {
 	b := &building.Builder{}
 	b.BuildFunction("resources/hellogo", "go1.x")
-
-	actual, err := os.ReadFile("resources/hellogo/main")
-	assert.NoError(t, err, "Failed to read actual Go binary built")
-
-	expected, err := os.ReadFile("resources/hellogo/expectedBinary")
-	assert.NoError(t, err, "Failed to read expected Go binary built")
-
-	assert.True(t, bytes.Equal(expected, actual), "Binary content mismatch")
+	_, err := os.Stat("./resources/hellogo/main")
+	assert.NoError(t, err)
 }
 
 func TestBuildFunctionUnsupported(t *testing.T) {
