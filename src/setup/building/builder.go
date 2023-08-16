d19c41ddd60c66ad013affbf5d148321dd3b7797
kk-min <76023265+kk-min@users.noreply.github.com>
2023-08-16 17:07:57 +0800

    Add return value (artifact path) to BuildFunction

--- src/setup/building/builder.go
+++ src/setup/building/builder.go
@@ -7,16 +7,18 @@ import (
 	"stellar/util"
 )
 
+const artifactDir = "setup/artifacts"
+
 // Builder struct keeps track of the functions built by stellar
 type Builder struct {
 	functionsBuilt map[string]bool
 }
 
-func (b *Builder) BuildFunction(provider string, functionName string, runtime string) {
+func (b *Builder) BuildFunction(provider string, functionName string, runtime string) string {
 	// First we check whether the function has not been built already
 	if b.functionsBuilt[functionName] {
 		log.Warnf("Function %s already built. Skipping.", functionName)
-		return
+		return fmt.Sprintf("%s/%s/%s.zip", artifactDir, functionName, functionName)
 	}
 
 	functionPath := fmt.Sprintf("setup/deployment/raw-code/serverless/%s/%s", provider, functionName)
@@ -28,9 +30,10 @@ func (b *Builder) BuildFunction(provider string, functionName string, runtime st
 	default:
 		// building not supported
 		log.Warnf("Building runtime %s is not necessary, or not supported. Continuing without building.", runtime)
-		return
+		return ""
 	}
 	b.functionsBuilt[functionName] = true
+	return fmt.Sprintf("%s/%s/%s.zip", artifactDir, functionName, functionName)
 }
 
 // buildJava builds the java zip artifact for serverless deployment using Gradle
