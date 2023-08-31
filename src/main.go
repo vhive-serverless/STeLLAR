e874663dd0614ab0fbefdda17698f8323048ed23
kk-min <76023265+kk-min@users.noreply.github.com>
2023-08-31 11:49:17 +0800

    Add removal of GCR services

--- src/main.go
+++ src/main.go
@@ -85,18 +85,11 @@ func main() {
 		benchmarking.TriggerSubExperiments(config, outputDirectoryPath, *specificExperimentFlag)
 
 		log.Info("Starting functions removal from cloud.")
-		slsRemoveMessage := setup.RemoveService(serverlessDirPath)
+		slsRemoveMessage := setup.RemoveService(config.Provider, serverlessDirPath)
 		log.Info(slsRemoveMessage)
 	} else {
 		setup.ProvisionFunctions(config)
 		benchmarking.TriggerSubExperiments(config, outputDirectoryPath, *specificExperimentFlag)
-	}
-
-	benchmarking.TriggerSubExperiments(config, outputDirectoryPath, *specificExperimentFlag, *writeToDatabaseFlag)
-
-	// Remove the functions used for experiments, since they are no longer needed
-	if *serverlessDeployment {
-		setup.RemoveService()
 	}
 
 	log.Infof("Done in %v, exiting...", time.Since(startTime))
