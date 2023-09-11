8aa9d3d24089d7fd3902aff82cb4d833f5292a01
Yi Pun Wong <wongyipun99@gmail.com>
2023-09-11 11:40:11 +0800

    change `TestDeployAndRemoveService` to do a substring search for keywords instead of number of lines

--- src/setup/integration-test/serverless-config_test.go
+++ src/setup/integration-test/serverless-config_test.go
@@ -1,7 +1,6 @@
 package setup
 
 import (
-	log "github.com/sirupsen/logrus"
 	"github.com/stretchr/testify/require"
 	"os/exec"
 	"stellar/setup"
@@ -13,16 +12,13 @@ import (
 // If this test is failing on your local machine, try running it with sudo.
 func TestDeployAndRemoveService(t *testing.T) {
 	// The two unit tests were merged together in order to make sure we are not left with a number of deployed test function on the cloud which are never used in.
+	assert := require.New(t)
+
 	util.RunCommandAndLog(exec.Command("cp", "test.yml", "../deployment/raw-code/serverless/aws/serverless.yml"))
 
 	msgDeploy := setup.DeployService("../deployment/raw-code/serverless/aws/")
+	msgRemove := setup.RemoveServiceAWS("../deployment/raw-code/serverless/aws/")
 
-	linesDeploy := len(strings.Split(msgDeploy, "\n"))
-
-	msgRemove := setup.RemoveService("../deployment/raw-code/serverless/aws/")
-	linesRemove := len(strings.Split(msgRemove, "\n"))
-	log.Info(msgDeploy)
-	log.Info(msgRemove)
-	require.Equal(t, 5, linesDeploy)
-	require.Equal(t, 1, linesRemove)
+	assert.True(strings.Contains(msgDeploy, "Service deployed"))
+	assert.True(strings.Contains(msgRemove, "successfully removed"))
 }
