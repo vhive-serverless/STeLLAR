9c22d09bb7717f4bbf12ad754a623ecd78c2f489
kk-min <76023265+kk-min@users.noreply.github.com>
2023-09-16 18:51:10 +0800

    Add hellonode function raw-code for Cloudflare

---
+++ src/setup/deployment/raw-code/serverless/cloudflare/hellonode/index.js
@@ -0,0 +1,32 @@
+export default {
+	async fetch(request) {
+		var incrLimit = 0
+
+		if (request.url.includes("incrementLimit")) {
+			incrLimit = parseInt(request.url.split("=")[1])
+		}
+
+		simulateWork(incrLimit)
+
+		const resData = {
+			"RequestID": "cloudflare-does-not-specify",
+			"TimestampChain": [str(Date.now())],
+		};
+
+		const body = JSON.stringify(resData, null, 2);
+
+		return new Response(body, {
+			headers: {
+				"content-type": "application/json;charset=UTF-8",
+			},
+		});
+	},
+};
+
+function simulateWork(incr) {
+	let i = 0;
+	while (i < incr) {
+		i++;
+	}
+}
+
