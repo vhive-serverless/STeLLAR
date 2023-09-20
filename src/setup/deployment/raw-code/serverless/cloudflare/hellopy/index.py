cd7c1acacac1cbae5dcd45f2c1fcdf19f389bb1c
kk-min <76023265+kk-min@users.noreply.github.com>
2023-09-20 19:37:32 +0800

    Update hellopy raw-code for Cloudflare

--- src/setup/deployment/raw-code/serverless/cloudflare/hellopy/index.py
+++ src/setup/deployment/raw-code/serverless/cloudflare/hellopy/index.py
@@ -1,6 +1,28 @@
+from datetime import datetime
 def handleRequest(request):
-    return __new__(Response('Python Worker hello world!', {
-        'headers' : { 'content-type' : 'text/plain' }
+    incr_limit = 0
+
+    if 'queryStringParameters' in request and 'IncrementLimit' in request['queryStringParameters']:
+        incr_limit = int(request['queryStringParameters'].get('IncrementLimit', 0))
+    elif 'body' in request and JSON.parse(request['body'])['IncrementLimit']:
+        incr_limit = int(JSON.parse(request['body'])['IncrementLimit'])
+
+    simulate_work(incr_limit)
+
+    response = JSON.stringify({
+        "RequestID": "cloudflare-does-not-specify",
+        "TimestampChain": [str(datetime.now())]
+    })
+
+
+    return __new__(Response(response, {
+        'headers' : { 'content-type' : 'application/json' }
     }))
+
+def simulate_work(increment):
+    # MAXNUM = 6103705
+    num = 0
+    while num < increment:
+        num += 1
 
 addEventListener('fetch', (lambda event: event.respondWith(handleRequest(event.request))))
