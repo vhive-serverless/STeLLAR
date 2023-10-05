49f5669fee6b71256ca7a988c4559d4d5b93b701
Yi Pun Wong <wongyipun99@gmail.com>
2023-10-05 14:01:36 +0800

    change Alibaba Cloud hellopy raw-code to HTTP handler format

--- src/setup/deployment/raw-code/serverless/aliyun/hellopy/main.py
+++ src/setup/deployment/raw-code/serverless/aliyun/hellopy/main.py
@@ -1,29 +1,42 @@
 import json
 import time
+import urllib.parse
+from io import BytesIO
+from typing import Dict, Any, List
 
 
-def main(event, context):
-    event = json.loads(event)
-
-    increment_limit = 0
-    if "IncrementLimit" in event["queryParameters"]:
-        increment_limit = int(event["queryParameters"]["IncrementLimit"])
-    if "IncrementLimit" in event["body"]:
-        increment_limit = int(event["body"]["IncrementLimit"])
+def main(environ, start_response):
+    increment_limit = get_increment_limit(environ)
     simulate_work(increment_limit)
 
-    response_body = {
+    status = "200 OK"
+    response_headers = [("Content-type", "application/json")]
+    context = environ.get("fc.context")
+    response_body = json.dumps({
         "Region": context.region,
         "RequestID": context.request_id,
         "TimestampChain": [str(time.time_ns())],
-    }
-    response = {
-        "isBase64Encoded": "false",
-        "statusCode": "200",
-        "headers": {"x-custom-header": "no", "Content-Type": "application/json"},
-        "body": response_body,
-    }
-    return json.dumps(response)
+    })
+    start_response(status, response_headers)
+    return [response_body]
+
+
+def get_increment_limit(environ) -> int:
+    increment_limit = 0
+
+    query_parameters: Dict[str, List[str]] = urllib.parse.parse_qs(environ.get("QUERY_STRING"))
+    if "IncrementLimit" in query_parameters:
+        increment_limit = int(query_parameters.get("IncrementLimit")[0])
+
+    content_length = int(environ.get("CONTENT_LENGTH", 0))
+    if content_length != 0:
+        request_body_buffer: BytesIO = environ.get("wsgi.input")
+        request_body_bytes: bytes = request_body_buffer.read(content_length)
+        request_body: Dict[str, Any] = json.loads(request_body_bytes)
+        if "IncrementLimit" in request_body:
+            increment_limit = request_body.get("IncrementLimit")
+
+    return increment_limit
 
 
 def simulate_work(increment_limit: int) -> None:
