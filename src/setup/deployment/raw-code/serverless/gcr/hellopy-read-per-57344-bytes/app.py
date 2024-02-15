e90cca32ffcb6a9925b900bda183ba47804dc669
Yi Pun Wong <wongyipun99@gmail.com>
2024-02-15 23:13:12 +0800

    Experiment 8 - Determine chunk size
    - AWS: Read 1B for every 4096B, 8192B, 16384B, 32768B, for exactly 1024 bytes
    - Azure: Read 1B for every 4096B, 8192B for exactly 1024 bytes
    - GCR: Read 1B for every 40kB, 48kB, 56kB for exactly 1024 bytes

---
+++ src/setup/deployment/raw-code/serverless/gcr/hellopy-read-per-57344-bytes/app.py
@@ -0,0 +1,46 @@
+import json
+import os
+import time
+from flask import Flask, request
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    incr_limit = 0
+    if request.args and 'incrementLimit' in request.args:
+        incr_limit = request.args.get('incrementLimit')
+
+    simulate_work(incr_limit)
+    read_filler_file("./filler.file")
+
+    response = {
+        "statusCode": 200,
+        "headers": {
+            "Content-Type": "application/json"
+        },
+        "body": {
+            "RequestID": "gcr-does-not-specify",
+            "TimestampChain": [str(time.time_ns())],
+        }
+    }
+
+    return json.dumps(response, indent=4)
+
+
+def simulate_work(incr):
+    num = 0
+    while num < incr:
+        num += 1
+
+
+def read_filler_file(path: str) -> None:
+    with open(path, 'rb') as f:
+        for i in range(1024):
+            f.seek(i * 57344)
+            f.read(1)
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
