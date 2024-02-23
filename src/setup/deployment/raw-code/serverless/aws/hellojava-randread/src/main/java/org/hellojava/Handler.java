bf652d6d44194010e7e129c10e22c7df8cd416a4
kk-min <76023265+kk-min@users.noreply.github.com>
2024-02-23 10:01:27 +0800

    exp: add hellojava-randread

---
+++ src/setup/deployment/raw-code/serverless/aws/hellojava-randread/src/main/java/org/hellojava/Handler.java
@@ -0,0 +1,95 @@
+package org.hellojava;
+
+import java.util.*;
+import java.time.Instant;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
+import com.google.gson.Gson;
+import org.crac.Resource;
+import org.crac.Core;
+
+class ResponseEventBody {
+    String region;
+    String requestId;
+    String[] timestampChain;
+
+    public ResponseEventBody(String region, String requestId, String[] timestampChain) {
+        this.region = region;
+        this.requestId = requestId;
+        this.timestampChain = timestampChain;
+    }
+}
+
+public class Handler implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent>, Resource{
+    static byte[] pageData;
+    @Override
+    public void beforeCheckpoint(org.crac.Context<? extends Resource> context) throws Exception {
+        pageData = new byte[4096 * 25600]; // 100 MB
+        new Random().nextBytes(pageData);
+    }
+
+    @Override
+    public void afterRestore(org.crac.Context<? extends Resource> context) {
+
+    }
+
+    @Override
+    public APIGatewayProxyResponseEvent handleRequest(APIGatewayProxyRequestEvent event, Context context)
+    {
+        Gson gson = new Gson();
+	int incrementLimit = 0;
+	if (event.getQueryStringParameters() != null) {
+        	incrementLimit = Integer.parseInt(event.getQueryStringParameters().getOrDefault("incrementLimit", "0"));
+	}
+        this.simulateWork(incrementLimit);
+        String requestId = "no-context";
+        if (context != null) {
+            requestId = context.getAwsRequestId();
+        }
+
+        long startTime = System.currentTimeMillis();
+        readDataRand();
+        long endTime = System.currentTimeMillis();
+
+        long pageReadTime = endTime-startTime;
+
+        String[] timestampChain = new String[]{""+pageReadTime};
+        ResponseEventBody resBody = new ResponseEventBody(System.getenv("AWS_REGION"), requestId, timestampChain);
+
+	Map<String, String> responseHeaders = new HashMap<>();
+	responseHeaders.put("Content-Type", "application/json");
+        APIGatewayProxyResponseEvent response = new APIGatewayProxyResponseEvent().withHeaders(responseHeaders);
+        response.setIsBase64Encoded(false);
+        response.setStatusCode(200);
+        response.setBody(gson.toJson(resBody));
+
+        return response;
+    }
+
+    public void simulateWork(int incrementLimit) {
+        int i = 0;
+        while (i < incrementLimit) {
+            i++;
+        }
+    }
+
+    public void readDataRand() {
+        int totalPages = pageData.length / 4096;
+        List<Integer> pages = IntStream.range(0, totalPages).boxed().collect(Collectors.toList());
+        Collections.shuffle(pages);
+        for (Integer pageNo : pages) {
+            readPage(pageNo);
+        }
+    }
+
+    public void readPage(int pageNo) {
+       for (int i = pageNo * 4096; i< (pageNo * 4096)+4096; i++) {
+           System.out.print(i);
+       }
+    }
+}
